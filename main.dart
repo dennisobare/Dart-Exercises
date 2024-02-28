3a3237f069c167b3ebb16db1a2ddb4b27bf19685
dennisobare <dennisobare338@gmail.com>
2024-02-28 14:17:55 +0300

    Changed the age to 78

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("Hello World!");
+  // declaring variables
+  String name = "Donald Trump"; // must have quotes
+  num age = 78; // must be a whole number
+  double height = 5.9; // has decimal places
+  bool isMarried = true; // either true or false
+  print("My name is $name");
+  print("I am $age years old");
+  print("My height is $height");
+  print("I am married: $isMarried");
+}
